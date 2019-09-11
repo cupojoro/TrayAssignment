b52195d45f002d70a2d2fe812e473d61225289c9
JoeRoss <joseph.ross@leanplum.com>
2019-09-11 14:26:30 -0700

    Adding Tray Assignment

---
+++ Readme.txt
@@ -0,0 +1,35 @@
+Roomba.js Instructions:
+
+Includes:
+	*Roomba.js
+	*StartPage.html
+	
+	*InputText.txt
+	
+	*InputText_Fail1.txt
+	*InputText_Fail2.txt
+	*InputText_Fail3.txt
+	*InputText_Fail4.txt
+	*InputText_Fail5.txt
+	
+	
+Integration:
+	To Properly Use the Roomba.js please include it as a script in a HTML file (one has been provided) and include
+	and input element that passes a text file to the function "processFile". Recommended event
+	handler is 'onchange'.
+
+Dependencies and Testing:
+	There are no dependencies for this file outside of an updated browser.
+	All testing was done on Chrome.
+
+Usage:
+	1. Once the html file has been created please include both the Roomba.js file and all the support text
+files into the same directory; or as required by your source path.
+	2. Open up the HTML file in your favorite browser and follow the onscreen prompt to choose a file. The "InputText.txt" file
+will run successfully where as all other .txt files included will test a failure point.
+	3. Open the inspector (on Chrome, right click the webpage and select it from the toolbar) and navigate to the console tab.
+	4. All outputs will be logged in the console.
+	
+Cheers!
+
+-Joe ross
---
+++ Roomba.js
@@ -0,0 +1,213 @@
+/**
+ * Helper class for create points in 2d space.
+ */
+class point
+{
+	constructor(inString)
+	{
+		let couple = inString.split(" ");
+		if (couple.length != 2)
+		{
+			throw("Input Point is not formatted properly");
+		}
+		try
+		{
+			if(couple[0].length == 0 || couple[1].length == 0)
+			{
+				throw("Value being parsed for point is empty.");
+			}
+			this.x = parseInt(couple[0], 10);
+			this.y = parseInt(couple[1], 10);
+		}catch(e)
+		{
+			throw("Value being parsed for point is not a number.");
+		}
+	}
+	
+	//I don't not believe ovverrides work by default.
+	toString()
+	{
+		return "(" + this.x + "," + this.y + ")";
+	}
+}
+
+/**
+ * The Roomba class processes a file containing location data and
+ * movement instructions. The Roomba class will then log the amount
+ * of spots cleaned and its final position.
+ */
+class Roomba
+{
+	
+	constructor()
+	{
+		this.fileContents = null;
+		this.dimensions = null;
+		this.startLoc = null;
+		this.currentLoc = null;
+		this.navString = null;
+		this.dirtLocations = null;
+		this.lookUpArray = null;
+		this.totalCleaned = 0;
+	}
+	
+	/**
+	 * Activate Roomba to process file and log total cleaned spots and final position.
+	 *
+	 * Parameters:
+	 * 	file: Blob
+	 * 		-Schema
+	 * 			row0: Dimensions
+	 * 			row1: Roomba start location
+	 * 			row[2-> (n-1)]: Dirt Locations
+	 * 			row[-1]: Movement instructions.
+	 */			
+	activate(file)
+	{
+		this.readFile(file).then(value => {
+			console.log("File Read Successfully.");
+			this.fileContents = value;
+			
+			this.parseInputText();
+			console.log("File Parsed Successfully. Running Roomba.");
+			
+			this.run();
+		}, reason => {
+			console.error(reason);
+		});
+	}
+	
+	/*
+	 * Itterate though the movement instructions, checking for dirt patches,
+	 * at completion log total cleaned and end spot.
+	 * Since the dirt patches are effectively hashed, the runtime for this should be
+	 * the fastest.
+	 */
+	run()
+	{
+		if (this.checkCurrentLocationForDirt())
+		{
+			this.totalCleaned += 1;
+		}
+		
+		[...this.navString].forEach((direction) => {
+			if (direction == "N" && this.currentLoc.y < this.dimensions.y)
+			{
+				this.currentLoc.y += 1;
+			}else if (direction == "S" && this.currentLoc.y > 0)
+			{
+				this.currentLoc.y -= 1;
+			}else if( direction == "W" && this.currentLoc.x > 0)
+			{
+				this.currentLoc.x -=1;
+			}else if(direction == "E" && this.currentLoc.x < this.dimensions.x)
+			{
+				this.currentLoc.x += 1;
+			}else
+			{
+				throw("Unknown Navigation: " + direction);
+			}
+			if (this.checkCurrentLocationForDirt())
+			{
+				this.totalCleaned += 1;
+			}
+		});
+		
+		console.log("End Location : (" + this.currentLoc.x + "," + this.currentLoc.y + ")");
+		console.log("Total Cleaned : " + this.totalCleaned);
+		
+	}
+	
+	/*
+	 *Helper function for mantaining the 'hashed' dirt locations.
+	 *Note: we remove an item once we clean it.
+	 */
+	checkCurrentLocationForDirt()
+	{
+		if ( this.lookUpArray[this.currentLoc.x][this.currentLoc.y])
+		{
+			this.lookUpArray[this.currentLoc.x][this.currentLoc.y] = null;
+			return true;
+		}else
+		{
+			return false;
+		}
+	}
+	
+	/**
+	 *The input file is read as a single string. This function parses down this string to its components.
+	 */
+	parseInputText()
+	{
+		let inputText = this.fileContents.split(/[\r\n]+/);
+		
+		if(inputText.length < 4)
+		{
+			throw("Input File does not have enough rows to be valid.");
+		}
+		
+		this.dimensions = new point(inputText[0]);
+		
+		if (this.dimensions.x <= 0 || this.dimensions.y <= 0)
+		{
+			throw("Dimensions cannot be 0 or negative. Input: " + this.dimensions.toString() );
+		}
+		//Setup the 'hash' table for quick lookups. For larger files this might be an issue in browsers.
+		this.lookUpArray = [...Array(this.dimensions.x)].map(e => Array(this.dimensions.y));
+		
+		this.startLoc = new point(inputText[1]);
+		this.currentLoc = this.startLoc;
+		
+		this.navString = inputText[inputText.length - 1];
+		
+		//Parse dirt locations and add them to 'hash' table.
+		this.dirtLocations = inputText.slice(2,inputText.length - 1).map((e) => {
+			let dirt = new point(e);
+			
+			if(dirt.x >= this.dimensions.x || dirt.y >= this.dimensions.y)
+			{
+				throw("Dirt Point is outside of bounds. Dirt Point : " + dirt.toString());
+			}
+			
+			this.lookUpArray[dirt.x][dirt.y] = dirt;
+			return dirt;
+		});
+	}
+	
+	/**
+	 *Create a promise to monitor the state of the file being read.
+	 *The culmination of this process will decide on terminating the process
+	 *or running the rest of the program.
+	 */
+	readFile(file)
+	{
+		let reader = new FileReader();
+		
+		let fileReadPromise = new Promise(
+			function(resolve, reject)
+			{
+				reader.onerror = function(e)
+				{
+					reject(new DOMException("Issue Reading File. Error Code : " + e.target.error.code));
+				};
+				
+				reader.onload = function(event)
+				{
+					resolve(event.target.result);
+				};
+				
+				reader.readAsText(file);
+			});
+		
+		return fileReadPromise;
+	}
+}
+
+/**
+ *Helper function to grab the file element from the HTML.
+ */
+function processFile(event)
+{
+	let roombaSolver = new Roomba();
+	roombaSolver.activate(event.target.files[0]);
+}
